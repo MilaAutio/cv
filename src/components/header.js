5886a26951b3ff99d4772ed50d777dde21c48579
Mila Autio <mila.autio@hotmail.com>
2024-03-09 17:27:38 +0200

    header created and styled

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return ( 
+    <header className='App-header'>
+        <div className="header-content">
+            <h1 className="page-title">CV</h1>
+        </div>
+    </header> )
+}
