65f784ec5aab2faa980f240185640956938c1d84
Mila Autio <mila.autio@hotmail.com>
2024-04-01 18:53:44 +0300

    added Content component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {Header} from './components/header';
+import {Content} from './components/page-content';
 
 function App() {
   return (
     <div className="App">
       {Header()}
+      {Content()}
     </div>
   );
 }
---
+++ src/components/page-content.js
@@ -0,0 +1,11 @@
+import {CVForm} from './cv-form'
+
+export const Content = () => {
+    return (
+        <div className="page-content">
+            <div className="content-area">
+                <CVForm />
+            </div>
+        </div>
+    );
+}
